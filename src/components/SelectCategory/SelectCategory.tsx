efeeef93296a54d8557188c1c399d5b87f55c2b9
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-04 00:50:53 -0300

    add search params by searchProducts for category

--- src/components/SelectCategory/SelectCategory.tsx
+++ src/components/SelectCategory/SelectCategory.tsx
@@ -1,17 +1,26 @@
 "use client";
 
 import { Category } from "@/@models";
+import { searchProducts } from "@/utils";
 import { FormControl, MenuItem, Select } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import { useRouter, useSearchParams } from "next/navigation";
 
 export function SelectCategory({ categories }: { categories: Category[] }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
   return (
     <FormControl size="small" sx={{ width: 200 }}>
       <Select
         name="select-category"
         sx={{ backgroundColor: grey[400] }}
-        onChange={(event) => {}}
+        value={searchParams.get("categoryId") || "0"}
+        onChange={(event) => {
+          const search = searchParams.get("search");
+          const categoryId = event.target.value as string;
+          searchProducts(router, search, categoryId);
+        }}
       >
         <MenuItem value="0">Todas as categorias</MenuItem>
         {categories.map((category) => (
