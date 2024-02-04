d0ff916524774979bb14c2c14c0f822de9373150
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-04 00:48:57 -0300

    create searchProducts func

--- src/utils/searchProducts.ts
+++ src/utils/searchProducts.ts
@@ -1,1 +1,25 @@
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+
+export function searchProducts(
+  router: AppRouterInstance,
+  search: string | undefined | null,
+  categoryId: string | undefined | null
+) {
+  let path = `/produtos`;
+
+  const urlSearchParams = new URLSearchParams();
+
+  if (search) {
+    urlSearchParams.append("search", search);
+  }
+
+  if (categoryId && categoryId !== "0") {
+    urlSearchParams.append("categoryId", categoryId);
+  }
+
+  if (urlSearchParams.toString()) {
+    path += `?${urlSearchParams.toString()}`;
+  }
+
+  router.push(path);
+}
