d473414a8139d3d91cb98c74c2674259a95afd99
Marcos Gomes <marcosgomesdev@gmail.com>
2024-02-04 00:48:05 -0300

    add configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/produtos",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
       },
     ],
   },
